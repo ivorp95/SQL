78475fe0ba712fe0bc71b43b336a193c1b685e7f
ivorp95 <ivor.pangos@live.com>
2023-11-22 17:43:41 +0100

    new file:   ipangos_SELECT_20231122.sql

---
+++ ipangos_SELECT_20231122.sql
@@ -0,0 +1,11 @@
+select NazivKupca, AdresaKupca from Kupci;
+select * from Kupci;
+SELECT MatBr from Kupci ;
+
+SELECT DISTINCT PostBr from Kupci;
+
+SELECT * from Kupci order by MatBr  desc ;
+
+select cijena from ArtiklRacun order by cijena desc;
+//sortianje po nekom atributu uzlazno ili silazno, sortiranje uvijek ide na kraj upita
+
